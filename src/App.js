201af36c81a6d230314d56b072f99e96cc873c0e
crazyhaller <suvigyamadrid@gmail.com>
2022-07-14 22:31:14 +0530

    Context,global state,provider and use context

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import{ v4 as uuidv4} from "uuid"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import {useState} from "react"
 import Header from "./Components/Header"
 import FeedbackList from "./Components/FeedbackList"
 import FeedbackStats from "./Components/FeedbackStats"
 import FeedbackForm from "./Components/FeedbackForm"
 import AboutIconLink from "./Components/AboutIconLink"
 import AboutPage from "./pages/AboutPage"
-import FeedbackData from "./Data/FeedbackData"
+import { FeedbackProvider } from "./context/FeedbackContext"
 
 function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback,...feedback])
-    }
-
-    const deleteFeedback = (id) =>{
-        if(window.confirm('Are you sure you want to delete?')){
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
-
     return (
+        <FeedbackProvider>
         <Router>
         <Header />
         <div className="container">
             <Routes>
                 <Route exact path="/" element={
                     <>
-                        <FeedbackForm handleAdd={addFeedback} />
-                    <FeedbackStats feedback={feedback}/>
-                    <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                    <FeedbackForm />
+                    <FeedbackStats />
+                    <FeedbackList />
                     </>
                 }>
                 </Route>           
@@ -41,6 +27,7 @@ function App(){
             <AboutIconLink />
         </div>        
         </Router>
+        </FeedbackProvider>
     )
 }
 
