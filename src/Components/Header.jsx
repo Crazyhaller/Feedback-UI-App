a62748ffbf4d6a3d78d06fe15407ac6778bebee1
crazyhaller <suvigyamadrid@gmail.com>
2022-07-14 23:17:16 +0530

    styles changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,7 +15,7 @@ function Header({text, bgColor, textColor}) {
 
 Header.defaultProps = {
     text: 'Feedback UI',
-    bgColor: 'rgba(0,0,0,0.4)',
+    bgColor: 'black',
     textColor: 'red'
 
 }
